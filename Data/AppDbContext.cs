d2a6c468797a8ddeda73f0433b04334022ba7e03
Quoc Danh <danh99171@gmail.com>
2025-04-09 11:50:31 +0700

    Commit 1:Code class user gồm các thuộc tính như thiết kế

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using QLDN.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLDN.Data
+{
+    internal class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("name=DefaultConnection") // chuỗi kết nối trong App.config
+        {
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLDN.Models
+{
+    internal class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+
+        public int FailedLoginCount { get; set; } = 0;
+
+        public DateTime? LockedUntil { get; set; }
+    }
+}
